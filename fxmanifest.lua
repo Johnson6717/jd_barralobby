53e18b0a112e8f7701a6ac3fa82a6fd28f22872e
Johnson6717 <97254477+Johnson6717@users.noreply.github.com>
2023-03-22 12:15:09 -0300

    marca de agua fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,4 @@
+-- Created by eleseta#3053 and Nico 1997#4637 // https://discord.gg/4ECThkQmaX
 fx_version "adamant"
 game "gta5"
 author "Nico 1997#4637"
@@ -5,3 +6,4 @@ author "Nico 1997#4637"
 client_scripts {
     'client/lobby.lua'
 }
+-- Created by eleseta#3053 and Nico 1997#4637 // https://discord.gg/4ECThkQmaX
