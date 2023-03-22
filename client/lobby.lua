d2a516d6fa37f99137a243497f6382b834cd6a36
Johnson6717 <97254477+Johnson6717@users.noreply.github.com>
2023-03-22 00:42:03 -0300

    coords

--- client/lobby.lua
+++ client/lobby.lua
@@ -2,7 +2,7 @@
 RegisterCommand("lobby", function()
 	notify("Te estas ~g~teletransportando~w~ al lobby")
 	Citizen.Wait(5000) -- 5 segundos
-    SetEntityCoords(PlayerPedId(), -413.423, 1168.692, 325.85 )
+    SetEntityCoords(PlayerPedId(), -413.423, 1168.692, 325.85 ) -- Cambiar coords a eleccion
     notify("Te has ~g~teletransportando~w~ al lobby")
 end, false)
 
