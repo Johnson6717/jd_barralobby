bb4a700a7cc2c4b7a0657de1611610566ae8f77f
Johnson6717 <97254477+Johnson6717@users.noreply.github.com>
2023-04-09 23:43:01 -0300

    fix bug - https://youtu.be/-ENdOKKEPBg
    before you could use the dead command, now you can't

--- client/lobby.lua
+++ client/lobby.lua
@@ -1,11 +1,15 @@
 -- Created by eleseta#3053 and Nico 1997#4637 // https://discord.gg/4ECThkQmaX
--- /lobby
+--- /lobby
 RegisterCommand("lobby", function()
-	notify("Te estas ~g~teletransportando~w~ al lobby")
-	Citizen.Wait(5000) -- 5 segundos
-    SetEntityCoords(PlayerPedId(), -413.423, 1168.692, 325.85 ) -- Cambiar coords a eleccion
-    notify("Te has ~g~teletransportando~w~ al lobby")
-end, false)
+    if not IsPlayerDead(PlayerId()) then
+        notify("Te estas ~g~teletransportando~w~ al lobby")
+        Citizen.Wait(5000) -- 5 segundos, lo pueden cambiar a gusto.
+        SetEntityCoords(PlayerPedId(), -413.423, 1168.692, 325.85 ) -- cambiar coords a gusto.
+        notify("Te has ~g~teletransportando~w~ al lobby")
+    else
+        notify("no puedes estas ~r~muerto")
+        end
+end)
 
 -- No Tocar
 function notify(text)
